95a1fcdeb58f2fd612f2cc9815af7f0dcaf6429e
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-05-01 23:53:33 +0300

    Make MainMenu UI

--- Game/Assets/_Source/core/Bootstrapper.cs
+++ Game/Assets/_Source/core/Bootstrapper.cs
@@ -1,5 +1,3 @@
-using LevelSystem;
-using TileSystem;
 using TimerSystem;
 using UnityEngine;
 
@@ -12,13 +10,6 @@ namespace core
         [SerializeField] private int addTime;
         [SerializeField] private int removeTime;
 
-        [Header("Level System")] 
-        [SerializeField] private GameObject firstTile;
-        [SerializeField] private GameObject transform;
-        [SerializeField] private Transform point;
-        [SerializeField] private int countSpawnTile;
-        [SerializeField] private Transform center;
-        
         private void Start()
         {
             Init();
@@ -27,7 +18,6 @@ namespace core
         private void Init()
         {
             StartCoroutine(new TimerController(time, addTime, removeTime).Check());
-            new Generation(transform).InstTiles(new ObjectPool(point), firstTile.transform.GetChild(0).GetComponent<Tile>(), countSpawnTile, center);
         }
     }
 }
